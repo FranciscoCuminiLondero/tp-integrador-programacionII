3a5e79e4f66e9a38422e76e9b683a38d53da4f36
Francisco <cuminifrancisco@gmail.com>
2024-09-16 18:33:53 -0300

    Modificando enrutado y modo oscuro

--- dark.light.js
+++ dark.light.js
@@ -1,25 +1,30 @@
-let boton = document.getElementById('boton-activo');
+let boton = document.getElementById("boton-activo");
 let body = document.body;
-let darkMode = localStorage.getItem('dark.mode');
+let darkMode = localStorage.getItem("dark-mode");
 
-
-function enableDarkMode(){
-    boton.classList.replace('fa-moon', 'fa-sun');
-    body.classList.add('dark');
-    localStorage.setItem('dark-mode', 'enabled');
+function enableDarkMode() {
+  boton.classList.replace("fa-moon", "fa-sun");
+  body.classList.add("dark");
+  localStorage.setItem("dark-mode", "enabled");
 }
 
-function disableDarkMode(){
-    boton.classList.replace('fa-sun', 'fa-moon');
-    body.classList.remove('dark');
-    localStorage.setItem('dark-mode', 'disabled');
+function disableDarkMode() {
+  boton.classList.replace("fa-sun", "fa-moon");
+  body.classList.remove("dark");
+  localStorage.setItem("dark-mode", "disabled");
+}
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+} else {
+  disableDarkMode();
 }
 
 boton.onclick = (e) => {
-    let darkMode = localStorage.getItem('dark-mode');
-    if(darkMode === 'disabled'){
-        enableDarkMode();
-    } else {
-        disableDarkMode();
-    }
-}
+  let darkMode = localStorage.getItem("dark-mode");
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+};
